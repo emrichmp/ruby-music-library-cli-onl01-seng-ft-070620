f9634124cc26a00d18379ed7196ad6b95f6d1539
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-31 17:51:24 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -60,13 +60,8 @@ class Song
     end
   end
   
-  def self.find_or_create_by_name(file)
-    array = filename.split("-")
-    name_of_song = array[1]
-    name_of_artist = array[0]
-    name_of_genre = array[2].split(".mp3").join
-    
-    artist = Artist.find_or_create_by_name
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
   end
   
 end
