4140bcd98ff6abd00a1130dd27eda29be8030d4e
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-31 18:42:08 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -45,9 +45,9 @@ class MusicLibraryController
   end
   
     def list_artists
-    Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |a, i|
-      puts "#{i+1}. #{a.name}"
+      Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |a, i|
+        puts "#{i+1}. #{a.name}"
+      end
     end
-  end
   
 end
