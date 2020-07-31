37bf6038bf7288722af12c66229a46b5a0a253cc
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-31 18:09:10 +0000

    Automatically backed up by Learn

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -3,4 +3,12 @@ class MusicImporter
   def initialize(path)
     @path = path
   end
+  
+  def files()
+    Dir.entries(@path).select! do 
+      entry.end_with?(".mp3")
+    end
+  end
+  
+  
 end
