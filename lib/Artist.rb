9d953286beaea61ee7740f56fd2a1c6247a45680
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-31 17:21:05 +0000

    Automatically backed up by Learn

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -1,24 +1,25 @@
 class Artist
-  attr_accessor :name, :song
-  
+  attr_accessor :name, :song, :musiclibrarycontroller, :musicimporter
+  extend Concerns::Findable
   @@all = []
-  
-  def intialize(name)
+
+  def initialize(name)
     @name = name
+    @songs = []
   end
-  
-  def self.all()
+
+  def self.all
     @@all
   end
-  
+
   def self.destroy_all
     @@all.clear
   end
-  
+
   def save
     @@all << self
   end
-  
+
   def self.create(artist)
     artist = self.new(artist)
     artist.save
