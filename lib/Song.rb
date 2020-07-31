d9f41d7090334a370037eb61207dcf29065d5e3d
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-31 17:09:31 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -1,1 +1,10 @@
 class Song
+  attr_accessor :name, :artist, :genre, :musicimporter, :musiclibrarycontroller
+  
+  @@all = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+  end
+  
+end
