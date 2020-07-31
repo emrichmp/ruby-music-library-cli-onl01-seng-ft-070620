c7773141d1f9e8f52f9c835141ddf21dfb36ed9b
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-31 17:19:20 +0000

    Automatically backed up by Learn

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -11,4 +11,18 @@ class Artist
   def self.all()
     @@all
   end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(artist)
+    artist = self.new(artist)
+    artist.save
+    artist
+  end
 end
